8729202862fcb7fd8bfcb6b5ce0b753b3b9f6c0b
David Jefts <dvdjefts27@gmail.com>
2019-02-05 04:14:59 -0500

    Finished NumA lab2 code and most of the diagrams

--- SMores/NumA/Lab 2/script.sh
+++ SMores/NumA/Lab 2/script.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/gnuplot
 
+set title "Graph of the function f(v)"
+
 set xzeroaxis linestyle 8 linewidth 2
 set xtics axis
 set xlabel "v"
 set ylabel "f(v)"
-plot "fort.7" with linespoints
+plot "fort.7" title "f(v)" with linespoints
+#plot "fort.7" every ::1 title "bisection" with linespoints, "fort.8" every ::1 title "false pos" with linespoints
 
 #set table "plot.tex"
 #plot "fort.7"
--- SMores/NumA/Lab 2/waals_equation.f90
+++ SMores/NumA/Lab 2/waals_equation.f90
@@ -3,7 +3,8 @@ program waals_equation
     double precision R, a, b, P, T, true
     double precision P1, P2, P3, T1, T2, T3
     integer n
-    double precision x_o, x_n, tol, err, r1, r2, step
+    double precision x_o, x_n, tol, err, r1, r2
+    double precision step, func
 
     !INITIALIZE VARIABLES
     R = 0.082054    !atm/(mol*K)
@@ -21,54 +22,69 @@ program waals_equation
     tol = 10.0**(-16)   !tolerance
     r1 = 2.0            !left solution range value
     r2 = 2.5            !right solution range value
-    x_n = (r1 + r2) / 2 !initial estimate
+    x_n = (r1 + r2)     !initial estimate
 
     !possible P and T values
     !P = 1, 10, 100
     !T = 300, 500, 700
 
-    !estimate the roots, maximum of 500 iterations
-    do n = 1, 500
+    !record basic function values from v = [2.0, 2.5]
+    do n = 1, 200
         !function:
         !waals(x_n, R, a, b, P, T)
 
-        x_o = x_n
-
-        !!!bisection method
-        x_n = (r1 + r2) / 2
-
-        !!!false position method
-        !x_n = (waals(r2, R, a, b, P, T) * r1 - waals(r1, R, a, b, P, T) * r1)
-        !x_n = x_n / (waals(r2, R, a, b, P, T) - waals(r1, R, a, b, P, T))
-
-        !calculate error and print
-        err = abs(x_o - x_n) !error
-        write(*, *) n, err, x_n
-
-        !!!!!!! Print all the things !!!!!!!
-        !!plot error vs time
-        write(7, *) n, err  !bisection
-        !write(8, *) n, err  !false pos
         !!plot f(v)
-        !step = 2 + (n * 0.001)
-        !write(7, *) step, waals(step, R, a, b, P, T)
-
-        !determine if error is small "enough"
-        if(err < tol .and. n > 1) then
-            write(*, *) "What I found: ", x_n
-            !do not know the true value
-            !write(*, *) "Actual value: ", true
-            write(*, *) n, " iterations"
-            stop
-        end if
-
-        if(waals(r1, R, a, b, P, T) * waals(x_n, R, a, b, P, T) < 0) then
-            r2 = x_n
-        end if
-        if(waals(r1, R, a, b, P, T) * waals(x_n, R, a, b, P, T) > 0) then
-            r1 = x_n
-        end if
+        step = 0.5 + (n * 0.01)
+        func = waals(step, R, a, b, P, T)
+        write(*, *) n, step, func
+        write(7, *) step, func
     end do
+
+    temp : do n = 300, 500, 700
+        pressure : do n = 1, 10, 100
+            !estimate the roots, maximum of 500 iterations
+            estimate : do n = 1, 500
+                !function:
+                !waals(x_n, R, a, b, P, T)
+
+                x_o = x_n
+
+                !!!bisection method
+                x_n = (r1 + r2) / 2
+
+                !!!false position method
+                !x_n = (waals(r2, R, a, b, P, T) * r1) - (waals(r1, R, a, b, P, T) * r2)
+                !x_n = x_n / (waals(r2, R, a, b, P, T) - waals(r1, R, a, b, P, T))
+
+                !calculate error and print
+                err = abs(x_o - x_n) !error
+                write(*, *) n, err, x_n
+
+                !!!!!!! Print all the things !!!!!!!
+                !!plot error vs time
+                !write(7, *) n, err  !bisection
+                !write(8, *) n, err  !false pos
+
+                !determine if error is small "enough"
+                if(err < tol .and. n > 1) then
+                    write(*, *) "What I found: ", x_n
+                    !do not know the true value
+                    !write(*, *) "Actual value: ", true
+                    write(*, *) n, " iterations"
+                    exit
+                end if
+
+                if(waals(r1, R, a, b, P, T) * waals(x_n, R, a, b, P, T) < 0) then
+                    r2 = x_n
+                end if
+                if(waals(r1, R, a, b, P, T) * waals(x_n, R, a, b, P, T) > 0) then
+                    r1 = x_n
+                end if
+            end do estimate
+        end do pressure
+    end do temp
+
+    CALL SYSTEM('gnuplot script.sh')
 
 contains
 
