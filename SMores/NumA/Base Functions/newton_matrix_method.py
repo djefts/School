499e3c79d8194eeaad415ad9b8d18f7bc33a1eeb
David Jefts <dvdjefts27@gmail.com>
2019-02-22 03:52:01 -0500

    Finished NumA lab4 report

--- SMores/NumA/Base Functions/newton_matrix_method.py
+++ SMores/NumA/Base Functions/newton_matrix_method.py
@@ -2,18 +2,23 @@ class Matrix:
     def __init__(self, matrix):
         self.matrix = matrix
         self.n = len(self.matrix)
-        self.sol = []
+        self.x = []
+        self.y = []
     
     def forward_elimination(self):
         for k in range(self.n - 1):  # loop through steps
             for i in range(k + 1, len(self.matrix)):  # loop through rows in bottom left corner
-                if self.matrix[k][k] == 0.0:
-                    print("pivoting")
-                    self.partial_pivoting(k, k)
                 m = self.matrix[i][k] / self.matrix[k][k]
-                for j in range(len(self.matrix) + 1):  # loop through columns
+                self.matrix[i][k] = m
+                for j in range(k + 1, len(self.matrix) + 1):  # loop through columns
                     self.matrix[i][j] = self.matrix[i][j] - m * self.matrix[k][j]
             print(self)
+    
+    def solveY(self):
+        pass
+    
+    def solveX(self):
+        pass
     
     def backward_substitution(self):
         sol = [0] * self.n
@@ -22,7 +27,7 @@ class Matrix:
             for col in range(row + 1, self.n):  # loop through columns
                 sum -= self.matrix[row][col] * sol[col]  # subtract each known value
             sol[row] = sum / self.matrix[row][row]  # divide by the diag value
-        self.sol = sol
+        self.x = sol
     
     def partial_pivoting(self, r, c):
         row = 0
@@ -49,8 +54,11 @@ class Matrix:
     def get_spot(self, i, j):
         return self.matrix[i][j]
     
-    def solution(self):
-        return '[' + ', '.join(['{}'.format(str(item)) for item in self.sol]) + ']\n'
+    def solve_matrix(self):
+        self.forward_elimination()
+        self.solveY()
+        self.solveX()
+        return '[' + ', '.join(['{}'.format(str(item)) for item in self.x]) + ']\n'
     
     def __str__(self):
         mx = max((len(str(i)) for row in self.matrix for i in row))
@@ -64,13 +72,13 @@ n = 4
 # -x - 2 * z = 1
 # 2 * x + 2 * y + 4 * z = 0
 
-# mat = Matrix([[1, 1, 0, 3, 4],
-#               [2, 1, -1, 1, 1],
-#               [3, -1, -1, 2, -3],
-#               [-1, 2, 3, -1, 4]])
-mat = Matrix([[1, -1, 3, -3],
-              [-1, 0, -2, 1],
-              [2, 2, 4, 0]])
+mat = Matrix([[1, 1, 0, 3, 8],
+              [2, 1, -1, 1, 7],
+              [3, -1, -1, 2, 14],
+              [-1, 2, 3, -1, -7]])
+# mat = Matrix([[1, -1, 3, -3],
+#               [-1, 0, -2, 1],
+#               [2, 2, 4, 0]])
 # mat = Matrix([[1, -5, 1, 7],
 #               [10, 0, 20, 6],
 #               [5, 0, -1, 4]])
@@ -79,4 +87,4 @@ print(mat, '\nForward Elimination:\n')
 mat.forward_elimination()  # WORKS!!
 print(mat, '\nBackward Substitution:\n')
 mat.backward_substitution()
-print(mat.solution())
+print(mat.solve_matrix())
