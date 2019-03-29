d0051c7a77b05b45377b23df3e1d972ea60e987f
David Jefts <dvdjefts27@gmail.com>
2019-03-29 13:47:22 -0400

    Finished AI wine analysis, moving on to diabetes regression

--- SMores/AI/Homework3/wineAnalysis.py
+++ SMores/AI/Homework3/wineAnalysis.py
@@ -1,71 +1,121 @@
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.impute import SimpleImputer
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, PolynomialFeatures
-from sklearn.model_selection import cross_val_score, cross_val_predict, GridSearchCV
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.svm import SVC, LinearSVC, SVR, LinearSVR
-from sklearn import datasets
-
+import numpy as np
+import pandas
+from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
-
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, accuracy_score, f1_score
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+# problem 1 models
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 
-import numpy as np
-import pandas as pd
-import os
 
-# Load in the wine data set
+def avg(array):
+    return float(sum(array)) / max(len(array), 1)
+
+
+def plot_learning_curves(model, x, y):
+    """
+    Plots performance on the training set and testing (validation) set.
+    X-axis - number of training samples used
+    Y-axis - RMSE
+    """
+    
+    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size = 0.20)
+    
+    training_errors, validation_errors = [], []
+    
+    for m in range(1, len(train_x)):
+        if m in range(0, 3):
+            continue
+        
+        model.fit(train_x[:m], train_y[:m])
+        
+        train_pred = model.predict(train_x)
+        test_pred = model.predict(test_x)
+        
+        training_errors.append(np.sqrt(mean_squared_error(train_y, train_pred)))
+        validation_errors.append(np.sqrt(mean_squared_error(test_y, test_pred)))
+    
+    plt.plot(training_errors, "r-+", label = "train")
+    plt.plot(validation_errors, "b-", label = "test")
+    plt.legend()
+    plt.axis([0, 80, 0, 3])
+
+
+# Import the wine data set
 wine = datasets.load_wine()
-print("Data:", wine.data, sep = '\n')
-print("\n\nTarget:", wine.target, sep = '\n')
 
+# Output the shape of the data
+data = wine.data
+pandas.set_option('display.max_columns', None)  # display every column
+pandas.set_option('display.expand_frame_repr', False)  # prevent line breaking
+pd_data = pandas.DataFrame(data = np.c_[wine['data'], wine['target']], columns = wine['feature_names'] + ['target'])
+print("Random Sample of Data:", pd_data.sample(10), sep = '\n')
+print("Shape of Data:", pd_data.shape)
 
-# Defining Data Pre-Processing Pipelines
-class DataFrameSelector(BaseEstimator, TransformerMixin):
-    
-    def __init__(self, attributes):
-        self.attributes = attributes
-    
-    def fit(self, X, y = None):
-        return self
-    
-    def transform(self, X):
-        return X[self.attributes]
+# Describe the minimum, maximum, and average values for each of the features and the target labels
+print("\nMinimum, Maximum, and Average values for each column of the dataset:")
+print(pd_data.agg([min, max, avg]))
+print("\n")
 
+# Implement a scatter matrix to determine if any features strongly correlate
+plt.tight_layout()  # <-- failed attempt to make the UserWarning go away
+scatter_matrix(pd_data, alpha = 0.2, figsize = (10, 10), c = wine.target)
+plt.show()
 
-class MostFrequentImputer(BaseEstimator, TransformerMixin):
-    
-    def fit(self, X, y = None):
-        self.most_frequent = pd.Series([X[c].value_counts().index[0] for c in X],
-                                       index = X.columns)
-        return self
-    
-    def transform(self, X):
-        return X.fillna(self.most_frequent)
+train_x, test_x, train_y, test_y = train_test_split(data, wine.target)
 
-# numeric_pipe = Pipeline([
-#     ("Select", DataFrameSelector(["Age", "Fare", "SibSp", "Parch"])),  # Selects Fields from dataframe
-#     ("Imputer", SimpleImputer(strategy = "median")),  # Fills in NaN w/ median value for its column
-# ])
-#
-# # Handle categorical string for sex by encoding as female true, 1 or false,0
-# train_df['Female'] = train_df["Sex"].apply(lambda x: 1 if x == 'female' else 0)
-#
-# categories_pipe = Pipeline([
-#     ("Select", DataFrameSelector(["Pclass", "Female"])),  # Selects Fields from dataframe
-#     ("MostFreqImp", MostFrequentImputer()),  # Fill in NaN with most frequent
-# ])
-#
-# preprocessing_pipe = FeatureUnion(transformer_list = [
-#     ("numeric pipeline", numeric_pipe),
-#     ("categories pipeline", categories_pipe)
-# ])
-#
-# # Process Input Data Using Pipleines
-# train_X_data = preprocessing_pipe.fit_transform(train_df)
-#
-# train_y_data = train_df["Survived"]
+print("\n\t\tK-Nearest Neighbors Classifier")
+k_nearest = KNeighborsClassifier(n_neighbors = 1)
+k_nearest.fit(train_x, train_y)
+pred_y = cross_val_predict(k_nearest, train_x, train_y, cv = 10)
+plt.figure("K-Nearest Neighbors")
+plot_learning_curves(k_nearest, train_x, train_y)
+plt.axis([0, 110, 0, 4])
+plt.show()
+print("Confusion Matrix:")
+print(confusion_matrix(train_y, pred_y))
+print("Accuracy Score = " + str(accuracy_score(train_y, pred_y)))
+print("Precision Score = " + str(precision_score(train_y, pred_y, average = 'macro')))
+print("Recall Score = " + str(recall_score(train_y, pred_y, average = 'macro')))
+print("F1 Score = " + str(f1_score(train_y, pred_y, average = 'macro')))
+print("MAE = " + str(mean_absolute_error(train_y, pred_y)))
+
+### Keep running into multiple errors with logistic regression incapable of determining that there actually IS 3
+### different classes
+# print("\n\t\tLogistic Regression Classifier")
+# log_reg = LogisticRegression(solver = 'lbfgs', max_iter = 5000, multi_class = 'auto')
+# log_reg.fit(train_x, train_y)
+# pred_y = log_reg.predict(test_x)
+# plt.figure("Logistic Regression")
+# plot_learning_curves(log_reg, train_x, train_y)
+# plt.axis([0, 110, 0, 4])
+# plt.show()
+# print("Confusion Matrix:")
+# print(confusion_matrix(train_y, pred_y))
+# print("Accuracy Score = " + str(accuracy_score(train_y, pred_y)))
+# print("Precision Score = " + str(precision_score(train_y, pred_y, average = 'macro')))
+# print("Recall Score = " + str(recall_score(train_y, pred_y, average = 'macro')))
+# print("F1 Score = " + str(f1_score(train_y, pred_y, average = 'macro')))
+# print("MAE = " + str(mean_absolute_error(train_y, pred_y)))
+
+print("\n\t\tLinear Support Vector Machine")
+lin_svm = LinearSVC(max_iter = 10000, dual=False, C=1)
+lin_svm.fit(train_x, train_y)
+pred_y = cross_val_predict(lin_svm, train_x, train_y, cv = 5)
+plt.figure("Linear SVM")
+plot_learning_curves(lin_svm, train_x, train_y)
+plt.axis([0, 110, 0, 4])
+plt.show()
+print("Confusion Matrix:")
+print(confusion_matrix(train_y, pred_y))
+print("Accuracy Score = " + str(accuracy_score(train_y, pred_y)))
+print("Precision Score = " + str(precision_score(train_y, pred_y, average = 'macro')))
+print("Recall Score = " + str(recall_score(train_y, pred_y, average = 'macro')))
+print("F1 Score = " + str(f1_score(train_y, pred_y, average = 'macro')))
+print("MAE = " + str(mean_absolute_error(train_y, pred_y)))
