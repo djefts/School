db96a90a3fcc348eec7a72ff179047cd5f02fd16
David Jefts <dvdjefts27@gmail.com>
2019-04-26 18:38:10 -0400

    NumA lab9 almost done

--- SMores/NumA/Base Functions/newton-cotes.py
+++ SMores/NumA/Base Functions/newton-cotes.py
@@ -2,8 +2,8 @@ from math import *
 
 
 def f(x):
-    # return 0.2 + 25 * x - 200 * x**2 + 675 * x**3 - 900 * x**4 + 400 * x**5
-    return exp(3 * x) * sin(2 * x)
+    return 0.2 + 25 * x - 200 * x**2 + 675 * x**3 - 900 * x**4 + 400 * x**5
+    # return exp(3 * x) * sin(2 * x)
 
 
 def trap_rule(n, h, x):
@@ -26,14 +26,16 @@ def simpson(n, h, x):
     return (h / 3) * (f(x[0]) + 4 * two + 2 * three + f(x[n]))
 
 
-a = 0
-b = pi / 4
-s = 100
-h = (b - a) / s
-n = s
+a = 0.0
+b = 0.8
+t = 2
+h = (b - a) / t
 x = [0]
-for i in range(s - 1):
+for i in range(t - 1):
     x.append(x[len(x) - 1] + h)
 print("%d x values:" % len(x), x)
-print("Expected:", 2.434782478661022043330862641718147463249065528328797272173)
-print("Actual:", simpson(n, h, x))
+# print("Expected:", 2.434782478661022043330862641718147463249065528328797272173)
+
+print("Expected:", 1.640533333)
+print("Trapezoid:", trap_rule(t, h, x))
+#print("Simpson:", simpson(t, h, x))
